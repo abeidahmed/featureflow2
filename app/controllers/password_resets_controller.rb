d71643b9ebefb47d7b0d5112815f95e6947ee04f
abeidahmed <abeidahmed92@gmail.com>
2021-07-02 22:15:29 +0530

    create demo password resets create action

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,4 +2,14 @@ class PasswordResetsController < ApplicationController
   layout "slate"
 
   def new; end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user
+      # do
+    else
+      render json: { errors: { invalid: ["email address"] } }, status: :unprocessable_entity
+    end
+  end
 end
--- spec/requests/password_resets_spec.rb
+++ spec/requests/password_resets_spec.rb
@@ -1,7 +1,23 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "PasswordResets", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  before do
+    host! "app.example.com"
+  end
+
+  describe "#create" do
+    context "when the email is present" do
+      it "sends the reset email" do
+      end
+    end
+
+    context "when the email is absent" do
+      it "returns an error" do
+        post password_resets_path, params: { email: "invalid@example.com" }
+
+        expect(json.dig(:errors, :invalid)).to be_present
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
   end
 end
