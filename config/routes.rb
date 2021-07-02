0642c5a9872f8775f5bd82fd403f9c8bc1689129
abeidahmed <abeidahmed92@gmail.com>
2021-07-02 17:22:48 +0530

    add sessions controller create action

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ end
 Rails.application.routes.draw do
   constraints AppSubdomainConstraint do
     resources :users, only: %i[create]
+    resources :sessions, only: %i[create]
   end
 end
---
+++ spec/requests/sessions_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe "Sessions", type: :request do
+  before do
+    host! "app.example.com"
+  end
+
+  describe "#create" do
+    context "when the request is valid" do
+      it "signs the user" do
+        user = create(:user)
+        post sessions_path, params: { email: user.email, password: user.password }
+
+        expect(signed_cookie[:auth_token]).to eq(user.auth_token)
+      end
+    end
+
+    context "when the request is invalid" do
+      it "does not sign the user" do
+        post sessions_path, params: { email: "invalid@example.com", password: "helloworld" }
+
+        expect(signed_cookie[:auth_token]).to be_nil
+        expect(json.dig(:errors, :invalid)).to be_present
+      end
+    end
+  end
+end
