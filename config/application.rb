239040670291450a5a46ba296abfc0a7d957f838
abeidahmed <abeidahmed92@gmail.com>
2021-07-02 17:38:21 +0530

    setup app to allow subdomains

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module Featureflow2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts = nil
 
     config.generators.system_tests = nil
     config.generators.stylesheets  = false
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   constraints AppSubdomainConstraint do
-    resources :users, only: %i[create]
+    resources :users, only: %i[new create]
     resources :sessions, only: %i[create]
   end
 end
