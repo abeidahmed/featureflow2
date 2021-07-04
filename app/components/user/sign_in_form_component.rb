ecb2e0fb8c74aced8d39d1482c46ea01a2971b38
abeidahmed <abeidahmed92@gmail.com>
2021-07-04 19:46:00 +0530

    style forms

--- app/components/user/sign_in_form_component.rb
+++ app/components/user/sign_in_form_component.rb
@@ -1,6 +1,7 @@
 class User
   class SignInFormComponent < ApplicationComponent
-    def initialize(**options)
+    def initialize(button_text: nil, **options)
+      @button_text = button_text.presence || "Sign in"
       @options = options
     end
   end
