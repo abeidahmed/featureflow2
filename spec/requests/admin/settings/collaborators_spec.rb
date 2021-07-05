ef89e04f8bd812700564d8d967f1e77ea2977214
abeidahmed <abeidahmed92@gmail.com>
2021-07-05 17:41:59 +0530

    test setting_collaborators_path

--- spec/requests/admin/settings/collaborators_spec.rb
+++ spec/requests/admin/settings/collaborators_spec.rb
@@ -1,7 +1,21 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Admin::Settings::Collaborators", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:account) { create(:account) }
+
+  before do
+    switch_account(account)
+    sign_in(user)
+  end
+
+  describe "#index" do
+    it "lists out the collaborators" do
+      create(:collaborator, user: user, account: account)
+      create_list(:collaborator, 5, account: account)
+      get setting_collaborators_path
+
+      expect(assigns(:collaborators).size).to eq(6)
+    end
   end
 end
