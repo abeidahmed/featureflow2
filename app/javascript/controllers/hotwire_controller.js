73655e9679c8dc9db9162f6f27e3609f8bdd6545
abeidahmed <abeidahmed92@gmail.com>
2021-07-05 22:13:24 +0530

    reset form after submit

--- app/javascript/controllers/hotwire_controller.js
+++ app/javascript/controllers/hotwire_controller.js
@@ -2,13 +2,27 @@ import { formErrorHandler } from "../helpers";
 
 export default class extends ApplicationController {
   static targets = ["errorContainer"];
+  static values = { resetAfterSubmit: Boolean };
 
   async onSubmit(event) {
     const formData = await event.detail.formSubmission;
     const { success, fetchResponse } = formData.result;
 
-    if (success) return;
+    if (success) {
+      this.resetForm();
+      return;
+    }
 
+    this.renderFormErrors(fetchResponse);
+  }
+
+  resetForm() {
+    if (this.element.hasAttribute("data-form-reset")) {
+      this.element.reset();
+    }
+  }
+
+  async renderFormErrors(fetchResponse) {
     const res = await fetchResponse.responseText;
     const { errors } = JSON.parse(res);
 
