a343efcaaa7550f980a052db5d8eb77a0e9c7779
abeidahmed <abeidahmed92@gmail.com>
2021-07-07 12:03:35 +0530

    fix production default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "https://app.featureflow.com", port: 3000 }
+  config.action_mailer.default_url_options = { host: "https://featureflow.com" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
