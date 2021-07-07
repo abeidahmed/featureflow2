d958d31acc521908ecf95c9c02e3d3055d28664e
abeidahmed <abeidahmed92@gmail.com>
2021-07-07 10:08:29 +0530

    add spec

--- spec/components/account/deactivate_component_spec.rb
+++ spec/components/account/deactivate_component_spec.rb
@@ -1,13 +1,38 @@
 require "rails_helper"
 
 RSpec.describe Account::DeactivateComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:account) { create(:account) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  before do
+    Current.account = account
+  end
+
+  after do
+    Current.account = nil
+  end
+
+  context "when the account is active" do
+    it "renders the confirmable input field" do
+      render_inline(described_class.new)
+
+      expect(rendered_component).to have_selector("[data-confirmable-confirm-value='#{account.name}']", visible: :hidden)
+      expect(rendered_component).to have_selector("input[data-confirmable-target='inputField']", visible: :hidden)
+    end
+
+    it "renders the submit button" do
+      render_inline(described_class.new)
+
+      expect(rendered_component).to have_selector("form[action='#{account_statuses_path}']", visible: :hidden)
+      expect(rendered_component).to have_selector("input[type='submit']", visible: :hidden)
+    end
+  end
+
+  context "when the account is inactive" do
+    it "does not render when account is inactive" do
+      account.inactive!
+      render_inline(described_class.new)
+
+      expect(rendered_component).to be_blank
+    end
+  end
 end
